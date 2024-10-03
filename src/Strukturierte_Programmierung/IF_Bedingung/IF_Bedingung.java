e7d47f6b4fa0b8ca77662f68c0292093824071e4
Darko15 <darko15pavlovic@gmail.com>
2024-10-03 09:45:10 +0200

    IF_Bedingung + Aufgabe_1 finished

--- src/Strukturierte_Programmierung/IF_Bedingung/IF_Bedingung.java
+++ src/Strukturierte_Programmierung/IF_Bedingung/IF_Bedingung.java
@@ -1,9 +1,11 @@
 package Strukturierte_Programmierung.IF_Bedingung;
 
+import java.util.Random;
+
 public class IF_Bedingung {
     public static void main(String[] args) {
         Random random = new Random();
-        int randomNumber = random.nextInt(bound: 100);
+        int randomNumber = random.nextInt(100);
 
         if((randomNumber>50) && (randomNumber<90)){
 
